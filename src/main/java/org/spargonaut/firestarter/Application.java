299604c94599d0ff795f16173d41ab8e028e166c
James Spargo <james.spargo@bytecubed.com>
2019-08-07 20:27:18 -0700

    JS - add demonstrations for time required for unboxing operations vs primitive type operations in main.

--- src/main/java/org/spargonaut/firestarter/Application.java
+++ src/main/java/org/spargonaut/firestarter/Application.java
@@ -3,5 +3,26 @@ package org.spargonaut.firestarter;
 public class Application {
     public static void main(String[] arguments) {
         System.out.println("Hello World!");
+
+//        sumReferenceTypes();
+        sumPrimitiveTypes();
+    }
+
+    private static void sumPrimitiveTypes() {
+        long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        System.out.println("Max int size: " + Integer.MAX_VALUE);
+        System.out.println("Primitive type sum: " + sum);
+    }
+
+    private static void sumReferenceTypes() {
+        Long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        System.out.println("Max int size: " + Integer.MAX_VALUE);
+        System.out.println("Reference type sum: " + sum);
     }
 }
