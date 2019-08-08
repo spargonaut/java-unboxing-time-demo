2c341a6c5cb1c65474f550999464da4c1a508f7c
James Spargo <james.spargo@bytecubed.com>
2019-08-07 20:26:34 -0700

    JS - add unit tests to demonstrate time required for unboxing operations vs primitive type operations.

--- src/test/java/org/spargonaut/firestarter/ApplicationTest.java
+++ src/test/java/org/spargonaut/firestarter/ApplicationTest.java
@@ -1,11 +1,13 @@
 package org.spargonaut.firestarter;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.boot.test.OutputCapture;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ApplicationTest {
 
@@ -15,6 +17,28 @@ public class ApplicationTest {
     @Test
     public void shouldPrintHelloWorldToStandardOut() {
         Application.main(null);
-        assertThat(capture.toString(), is("Hello World!\n"));
+        assertTrue(capture.toString().contains("Hello World!\n"));
+    }
+
+    @Test
+    @Ignore("Unignore this test to demonstate the time require to sum reference types")
+    public void shouldDemonstrateTheTimeRequiredForSummingReferenceTypes() {
+        Long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        System.out.println("Max int size: " + Integer.MAX_VALUE);
+        System.out.println("Reference type sum: " + sum);
+    }
+
+    @Test
+    @Ignore("Unignore this test to demonstate the time require to sum primitive types")
+    public void shouldDemonstrateTheTimeRequiredForSummingPrimitiveTypes() {
+        long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        System.out.println("Max int size: " + Integer.MAX_VALUE);
+        System.out.println("Primitive type sum: " + sum);
     }
 }
